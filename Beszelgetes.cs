db629ddbea6760c9588471d946203b4879f1edcf
horvath.krisztian2 <horvath.krisztian2@28-16>
2022-12-02 15:10:26 +0100

    Add project files.

---
+++ Beszelgetes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cseveges
+{
+    internal class Beszelgetes
+    {
+        public DateTime Kezdet { get; set; }
+        public DateTime Veg { get; set; }
+        public string Kezdemenyezo { get; set; }
+        public string Fogado { get; set; }
+
+        public Beszelgetes(DateTime kezdet, DateTime veg, string kezdemenyezo, string fogado)
+        {
+            Kezdet = kezdet;
+            Veg = veg;
+            Kezdemenyezo = kezdemenyezo;
+            Fogado = fogado;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace Cseveges
+{
+    internal class Program
+    {
+        static List<Beszelgetes> Beszelgetesek = new();
+        static List<string> Tagok = new();
+        static void Main(string[] args)
+        {
+            Feladat3a();
+            Feladat3b();
+            Feladat4();
+            Feladat5();
+            Feladat6();
+            Feladat7();
+            Feladat8();
+        }
+        static void Feladat8()
+        { 
+            
+        }
+        static void Feladat7()
+        {
+            Console.WriteLine("Tagok akik nem beszéltek senkivel:");
+            foreach (var tag in Tagok)
+            {
+                int Alkalom = Beszelgetesek.Count(x => x.Kezdemenyezo == tag || x.Fogado == tag);
+                if (Alkalom == 0)
+                {
+                    Console.WriteLine($"{tag}");
+                }
+            }
+        }
+        static void Feladat6()
+        {
+            Console.Write("Kérek egy tag nevét: ");
+            string Nev = Console.ReadLine();
+            TimeSpan OsszesIdo = new TimeSpan(0, 0, 0);
+            foreach (var beszelgetes in Beszelgetesek)
+            {
+                if (beszelgetes.Fogado == Nev || beszelgetes.Kezdemenyezo == Nev)
+                {
+
+                    TimeSpan kulonbseg = beszelgetes.Veg.Subtract(beszelgetes.Kezdet);
+                    OsszesIdo += kulonbseg;
+                }
+            }
+            Console.WriteLine($"Általa beszélt idő: {String.Format(OsszesIdo.ToString(),"00.00.00")}");
+        }
+        static void Feladat5()
+        {
+            TimeSpan Ido = new TimeSpan(0,0,0);
+            Beszelgetes Leghosszabb = null;
+            foreach (var beszelgetes in Beszelgetesek)
+            {
+                TimeSpan kulonbseg = beszelgetes.Veg.Subtract(beszelgetes.Kezdet);
+                if (kulonbseg > Ido)
+                {
+                    Ido = kulonbseg;
+                    Leghosszabb = beszelgetes;
+                }
+            }
+            Console.WriteLine("Leghosszabb beszélgetés:");
+            Console.WriteLine($"\tHossza: {String.Format(Ido.ToString(),"{00.00.00}")}");
+            Console.WriteLine($"\tKezdet idő: {Leghosszabb.Kezdet.ToString()}");
+            Console.WriteLine($"\tVég:        {Leghosszabb.Veg.ToString()}");
+            Console.WriteLine($"\tKezedeményező: {Leghosszabb.Kezdemenyezo}");
+            Console.WriteLine($"\tFogadó:        {Leghosszabb.Fogado}");
+        }
+        static void Feladat4()
+        {
+            foreach (var tag in Tagok)
+            {
+                Console.WriteLine($"{tag} \t" + Beszelgetesek.Count(x => x.Kezdemenyezo == tag || x.Fogado == tag));
+            }
+        }
+        static void Feladat3b()
+        {
+            using StreamReader sr = new(@"..\..\..\src\tagok.txt");
+            _ = sr;
+            int Darab = 0;
+            while (!sr.EndOfStream)
+            {
+                string Sor = sr.ReadLine();
+                Tagok.Add(Sor);
+                Darab++;
+            }
+            Console.WriteLine($"Beolvasva {Darab}db tag");
+        }
+        static void Feladat3a()
+        {
+            using StreamReader sr = new(@"..\..\..\src\csevegesek.txt");
+            _ = sr;
+            int Darab = 0;
+            while (!sr.EndOfStream)
+            {
+                string Sor = sr.ReadLine();
+                string[] Tulajdonsagok = Sor.Split(';');                
+                try
+                {
+                    DateTime kezdet = DateTime.ParseExact(Tulajdonsagok[0], "yy.MM.dd-HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+                    DateTime veg = DateTime.ParseExact(Tulajdonsagok[1], "yy.MM.dd-HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+                    string kezdemenyezo = Tulajdonsagok[2];
+                    string fogado = Tulajdonsagok[3];
+                    Beszelgetesek.Add(new Beszelgetes(kezdet, veg, kezdemenyezo, fogado));
+                    Darab++;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Hibás sor kezelve");
+                }
+            }
+            Console.WriteLine($"Beolvasva {Darab}db csevegés");
+        }
+    }
+}
