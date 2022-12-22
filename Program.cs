fc24a667b899759161f137d25a5d8e6b1030a266
Krisztian05 <horvath.krisztian2@verebelyszki.hu>
2022-12-22 12:44:17 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Globalization;
-using System.Runtime.CompilerServices;
 
 namespace Cseveges
 {
@@ -16,78 +15,6 @@ namespace Cseveges
             Feladat6();
             Feladat7();
             Feladat8();
-        }
-        static void Feladat8()
-        { 
-            
-        }
-        static void Feladat7()
-        {
-            Console.WriteLine("Tagok akik nem beszéltek senkivel:");
-            foreach (var tag in Tagok)
-            {
-                int Alkalom = Beszelgetesek.Count(x => x.Kezdemenyezo == tag || x.Fogado == tag);
-                if (Alkalom == 0)
-                {
-                    Console.WriteLine($"{tag}");
-                }
-            }
-        }
-        static void Feladat6()
-        {
-            Console.Write("Kérek egy tag nevét: ");
-            string Nev = Console.ReadLine();
-            TimeSpan OsszesIdo = new TimeSpan(0, 0, 0);
-            foreach (var beszelgetes in Beszelgetesek)
-            {
-                if (beszelgetes.Fogado == Nev || beszelgetes.Kezdemenyezo == Nev)
-                {
-
-                    TimeSpan kulonbseg = beszelgetes.Veg.Subtract(beszelgetes.Kezdet);
-                    OsszesIdo += kulonbseg;
-                }
-            }
-            Console.WriteLine($"Általa beszélt idő: {String.Format(OsszesIdo.ToString(),"00.00.00")}");
-        }
-        static void Feladat5()
-        {
-            TimeSpan Ido = new TimeSpan(0,0,0);
-            Beszelgetes Leghosszabb = null;
-            foreach (var beszelgetes in Beszelgetesek)
-            {
-                TimeSpan kulonbseg = beszelgetes.Veg.Subtract(beszelgetes.Kezdet);
-                if (kulonbseg > Ido)
-                {
-                    Ido = kulonbseg;
-                    Leghosszabb = beszelgetes;
-                }
-            }
-            Console.WriteLine("Leghosszabb beszélgetés:");
-            Console.WriteLine($"\tHossza: {String.Format(Ido.ToString(),"{00.00.00}")}");
-            Console.WriteLine($"\tKezdet idő: {Leghosszabb.Kezdet.ToString()}");
-            Console.WriteLine($"\tVég:        {Leghosszabb.Veg.ToString()}");
-            Console.WriteLine($"\tKezedeményező: {Leghosszabb.Kezdemenyezo}");
-            Console.WriteLine($"\tFogadó:        {Leghosszabb.Fogado}");
-        }
-        static void Feladat4()
-        {
-            foreach (var tag in Tagok)
-            {
-                Console.WriteLine($"{tag} \t" + Beszelgetesek.Count(x => x.Kezdemenyezo == tag || x.Fogado == tag));
-            }
-        }
-        static void Feladat3b()
-        {
-            using StreamReader sr = new(@"..\..\..\src\tagok.txt");
-            _ = sr;
-            int Darab = 0;
-            while (!sr.EndOfStream)
-            {
-                string Sor = sr.ReadLine();
-                Tagok.Add(Sor);
-                Darab++;
-            }
-            Console.WriteLine($"Beolvasva {Darab}db tag");
         }
         static void Feladat3a()
         {
@@ -109,10 +36,104 @@ namespace Cseveges
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("Hibás sor kezelve");
+                    //Console.WriteLine("Hibás sor kezelve");
                 }
             }
-            Console.WriteLine($"Beolvasva {Darab}db csevegés");
+            //Console.WriteLine($"Beolvasva {Darab}db csevegés");
+        }
+        static void Feladat3b()
+        {
+            using StreamReader sr = new(@"..\..\..\src\tagok.txt");
+            _ = sr;
+            int Darab = 0;
+            while (!sr.EndOfStream)
+            {
+                string Sor = sr.ReadLine();
+                Tagok.Add(Sor);
+                Darab++;
+            }
+            //Console.WriteLine($"Beolvasva {Darab}db tag");
+        }
+        static void Feladat4()
+        {
+            foreach (var tag in Tagok)
+            {
+                Console.WriteLine($"{tag} \t" + Beszelgetesek.Count(x => x.Kezdemenyezo == tag || x.Fogado == tag));
+            }
+        }
+        static void Feladat5()
+        {
+            TimeSpan Ido = new TimeSpan(0,0,0);
+            Beszelgetes Leghosszabb = null;
+            foreach (var beszelgetes in Beszelgetesek)
+            {
+                TimeSpan kulonbseg = beszelgetes.Veg.Subtract(beszelgetes.Kezdet);
+                if (kulonbseg > Ido)
+                {
+                    Ido = kulonbseg;
+                    Leghosszabb = beszelgetes;
+                }
+            }
+            Console.WriteLine("5. feladat: Leghosszabb beszélgetés:");
+            Console.WriteLine($"\tHossza: {String.Format(Ido.ToString(),"{00.00.00}")}");
+            Console.WriteLine($"\tKezdet idő: {Leghosszabb.Kezdet.ToString()}");
+            Console.WriteLine($"\tVég:        {Leghosszabb.Veg.ToString()}");
+            Console.WriteLine($"\tKezedeményező: {Leghosszabb.Kezdemenyezo}");
+            Console.WriteLine($"\tFogadó:        {Leghosszabb.Fogado}");
+        }
+        static void Feladat6()
+        {
+            Console.Write("6. feladat: Kérek egy tag nevét: ");
+            string Nev = Console.ReadLine();
+            TimeSpan OsszesIdo = new TimeSpan(0, 0, 0);
+            foreach (var beszelgetes in Beszelgetesek)
+            {
+                if (beszelgetes.Fogado == Nev || beszelgetes.Kezdemenyezo == Nev)
+                {
+
+                    TimeSpan kulonbseg = beszelgetes.Veg.Subtract(beszelgetes.Kezdet);
+                    OsszesIdo += kulonbseg;
+                }
+            }
+            Console.WriteLine($"Általa beszélt idő: {String.Format(OsszesIdo.ToString(),"00.00.00")}");
+        }
+        static void Feladat7()
+        {
+            Console.WriteLine("7. feladat: Tagok akik nem beszéltek senkivel:");
+            foreach (var tag in Tagok)
+            {
+                int Alkalom = Beszelgetesek.Count(x => x.Kezdemenyezo == tag || x.Fogado == tag);
+                if (Alkalom == 0)
+                {
+                    Console.WriteLine($"{tag}");
+                }
+            }
+        }
+        static void Feladat8()
+        {
+            Console.WriteLine("8. feladat: Leghosszabb csendes időszak 15h-tól");
+            DateTime maxCsendKezdete = new DateTime(2021, 9, 27, 15, 0, 0);
+            DateTime maxCsendVege = Beszelgetesek[0].Kezdet;
+            TimeSpan maxCsendHossz = maxCsendVege - maxCsendKezdete;
+            DateTime aktCsendVege = Beszelgetesek[0].Veg;
+
+            foreach (var b in Beszelgetesek.Skip(1))
+            {
+                if (b.Kezdet > aktCsendVege)   // Ha csend van:
+                {
+                    TimeSpan aktCsendHossz = b.Kezdet - aktCsendVege;
+                    if (aktCsendHossz > maxCsendHossz)
+                    {
+                        maxCsendHossz = aktCsendHossz;
+                        maxCsendKezdete = aktCsendVege;
+                        maxCsendVege = b.Kezdet;
+                    }
+                }
+                if (b.Veg > aktCsendVege) aktCsendVege = b.Veg;
+            }
+            Console.WriteLine($"\tKezdete: {maxCsendKezdete.ToString("yy.MM.dd-HH:mm:ss")}");
+            Console.WriteLine($"\tVége:    {maxCsendVege.ToString("yy.MM.dd-HH:mm:ss")}");
+            Console.WriteLine($"\tHossza:  {maxCsendHossz}");
         }
     }
 }
